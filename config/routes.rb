60ee90bf1175470f08b26b8314e6468bdd14d799
jieming Mao <maojieming@gmail.com>
2018-05-29 14:58:45 +0800

    topics init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'topics#index'
+  resources :topics
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
