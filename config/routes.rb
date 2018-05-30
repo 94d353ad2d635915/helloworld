0cbebff339d041c0a9f7f92891a77a835b5d0bb5
jieming Mao <maojieming@gmail.com>
2018-05-30 17:35:25 +0800

    remove unused topic comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'topics#index'
   resources :topics do 
-    resources :comments, only: [:create, :update, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
