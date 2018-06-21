e290323f0bf985acfc26dd7f6e4e74a28f175577
jieming Mao <maojieming@gmail.com>
2018-06-22 04:00:26 +0800

    [fixed] remove profile destroy and all_blank_destroy

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,11 @@ class ApplicationRecord < ActiveRecord::Base
 
   # accepts_nested_attributes_for ..., reject_if: :all_blank_destroy
   def all_blank_destroy(attributes)
-    attributes.merge!(_destroy: true) if attributes.select{|k,v| !v.blank?}.empty?
-    false
+    attributes_empty = attributes.select{|k,v| !v.blank?}.empty?
+    if self.valid? and attributes_empty
+      attributes.merge!(_destroy: true)
+      return false
+    end
+    attributes_empty
   end
 end
