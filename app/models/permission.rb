1947214412a1f847ffc39b3ce28c510756ffc380
jieming Mao <maojieming@gmail.com>
2018-06-24 22:51:26 +0800

    [fixed] delete permission keep event, set event.permission_id nullify

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -4,7 +4,7 @@ class Permission < ApplicationRecord
   # extra attributes
   has_many :assign_permissions_roles, dependent: :destroy
   has_many :roles, through: :assign_permissions_roles
-  has_one :event#操作删除，事件保留，保留日志
+  has_one :event, dependent: :nullify#操作删除，事件保留，保留日志
 
   after_commit {Rails.cache.delete("#{self.class}_all")}
 end
